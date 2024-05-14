10ae7ff442e04a5b03a0fd81a0ac861df41d493d
Jess Mattless <jessmattless2002@gmail.com>
2024-05-14 22:44:43 +0100

    Delete set_known_hosts.sh

--- set_known_hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ssh-keyscan -t rsa 172.18.0.2 >> ~/.ssh/known_hosts
-ssh-keyscan -t rsa 172.18.0.3 >> ~/.ssh/known_hosts
-ssh-keyscan -t rsa 172.18.0.4 >> ~/.ssh/known_hosts
-ssh-keyscan -t rsa 172.18.0.5 >> ~/.ssh/known_hosts
